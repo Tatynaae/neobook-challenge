3a98b8b316ef9b973b42c87f430c0d3d60edd262
Tatyna <tatynaerkinbek@gmailcom>
2023-12-18 22:08:29 +0600

    dev2/routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.scss";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Login from "./pages/login";
+
+import "./App.scss";
+
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
