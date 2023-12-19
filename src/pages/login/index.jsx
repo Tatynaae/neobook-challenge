6a0514e8a754ee213e4b9857e4f60d2d0d9661d9
Tatyna <tatynaerkinbek@gmailcom>
2023-12-19 20:04:35 +0600

    dev2/add-formik-vs-yup

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,88 +1,21 @@
 import React from "react";
-
-import { Input } from "../../components/ui/Input/Input";
-import banner from "../../assets/images/image 1.png";
-import Basket from "../../assets/icons/basket";
-import Button from "../../components/ui/Button/Button";
-import Closed from "../../assets/icons/closed";
+import Banner from "./banner";
+import MyForm from "../../components/formik";
 
 import "./index.scss";
 
-import { useDispatch, useSelector } from "react-redux";
-import { selectAuth, setPassword, setUsername } from "../../store/authSlice";
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectAuth, setPassword, setUsername } from "../../store/authSlice";
 
 const Login = () => {
-  const [open, setOpen] = React.useState(false);
-  // const [validData, setValidData] = React.useState(false);
-
-  const dispatch = useDispatch();
-  const { username, password } = useSelector(selectAuth);
-
-  const handleUsernameChange = (e) => {
-    dispatch(setUsername(e.target.value));
-  };
-
-  const handlePasswordChange = (e) => {
-    dispatch(setPassword(e.target.value));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  // const checkUserDate = () => {
-  //   if (username === "Tatyna" && password === "neobis2023") {
-  //     setValidData(true);
-  //   } else {
-  //     setValidData(false);
-  //   }
-  // }
+  // const dispatch = useDispatch();
+  // const { username, password } = useSelector(selectAuth);
 
   return (
     <div className="login">
-      <div className="banner">
-        <img src={banner} alt="#" />
-        <div className="rec"></div>
-        <div className="basket">
-          <Basket />
-        </div>
-      </div>
+        <Banner />
       <div className="enter">
-        <div className="top-enter">
-          <form className="form" onSubmit={handleSubmit}>
-            <Input
-              type={"text"}
-              placeholder={"Имя пользователя"}
-              variant="default"
-              label={"Имя пользователя"}
-              onChange={handleUsernameChange}
-              value={username}
-            />
-
-            <Input
-              type={open ? "text" : "password"}
-              placeholder={"Пароль"}
-              label={"Пароль"}
-              icon={<Closed open={open} setOpen={setOpen} />}
-              onChange={handlePasswordChange}
-              value={password}
-            />
-
-            <Button
-              text={"Забыли пароль"}
-              variant="link"
-              className={"linked"}
-            />
-            <Button
-              type="submit"
-              text={"Войти"}
-              variant={username && password ? "default" : "disabled"}
-              className={"enter-btn"}
-            />
-          </form>
-
-          <Button text={"Зарегистрироваться"} variant="link" />
-        </div>
+        <MyForm />
       </div>
     </div>
   );
